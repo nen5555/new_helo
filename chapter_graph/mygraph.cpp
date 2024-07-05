6a6c7ede7ab1c7f6a56dcb7857ddac05f29a3ac0
linhao <446010712@qq.com>
2024-07-05 18:57:39 +0800

    add the all change

--- chapter_graph/mygraph.cpp
+++ chapter_graph/mygraph.cpp
@@ -1,4 +1,44 @@
 #include "../utils/common.hpp"
+#include "./graph_adjacency_list.cpp"
+vector<Vertex *> graphbfs(GraphAdjList &graph,Vertex *startVex){
+
+vector<Vertex *> res;
+unordered_set<Vertex *> visited = {startVex};
+queue<Vertex *> que;
+que.push(startVex);
+
+while(!que.empty()){
+    Vertex *vet = que.front();
+    que.pop();
+    res.push_back(vet);
+    for(auto adjvet : graph.adjList[vet]){
+        if(visited.count(adjvet))
+            continue;
+        que.push(adjvet);
+        visited.emplace(adjvet);
+    }
+
+}
+    return res;
+}
+
+void dfs(GraphAdjList &graph,unordered_set<Vertex *> &visited,vector<Vertex *> &res,Vertex *vex){
+    res.push_back(vex);
+    visited.emplace(vex);
+
+    for(Vertex *adjvex:graph.adjList[vex]){
+        if(visited.count(adjvex))
+        continue;
+        dfs(graph,visited,res,adjvex);
+    }
+}
+vector<Vertex *>graphdfs(GraphAdjList &graph,Vertex *startVex){
+    vector<Vertex*> res;
+    unordered_set<Vertex *> visited;
+    dfs(graph,visited,res,startVex);
+
+    return res; 
+}
 class graphmat{
     vector<int> vertices;
     vector<vector<int>> adjmat;
@@ -63,7 +103,29 @@ int main(){
     cout << "\n初始化后，图为" << endl;
     graph.print();
 
-    return 0;
+    vector<Vertex *> v =valsToVets({1,2,3,4,5,6,7,8,9});
+    vector<vector<Vertex *>> edges_v =  {{v[0], v[1]}, {v[0], v[3]}, {v[1], v[2]}, {v[1], v[4]},
+                                      {v[2], v[5]}, {v[3], v[4]}, {v[3], v[6]}, {v[4], v[5]},
+                                      {v[4], v[7]}, {v[5], v[8]}, {v[6], v[7]}, {v[7], v[8]}};
+
+    GraphAdjList graph_v(edges_v);
+    cout << "\n初始化后，图为\\n";
+    graph_v.print();
+
+    /* 广度优先遍历 */
+    vector<Vertex *> res = graphbfs(graph_v, v[0]);
+    cout << "\n广度优先遍历（BFS）顶点序列为" << endl;
+    printVector(vetsToVals(res));
+    /*深度优先遍历*/
+    vector<Vertex *> resdfs = graphdfs(graph_v, v[0]);
+    cout << "\n深度优先遍历（DFS）顶点序列为" << endl;
+    printVector(vetsToVals(resdfs));
+
+    // 释放内存
+    for (Vertex *vet : v) {
+        delete vet;
+    }
+
 
 
 }
